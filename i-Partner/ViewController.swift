4d9f04eba136e8f1ed8204d850f33d8a7ccc65c9
Manuel <naruto06@mail.ru>
2016-10-03 15:13:22 +0300

    Initial Commit

---
+++ i-Partner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i-Partner
+//
+//  Created by Мануэль on 03.10.16.
+//  Copyright © 2016 AuroraInterplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
